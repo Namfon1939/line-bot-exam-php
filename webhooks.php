a83cfb95f332f6db190259b2f496e0bad298c6ee
Supapit Mekheed <Supapit.m@avalue.co.th>
2020-03-05 11:49:03 +0700

    update access token

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'QychfE1EJmMXXPlIc1r2POjrjG9+aZm1mueegxanDbMDmOF+LYYCWo7jACB01mi6JtNeePsSl8kEiSGt4VOIUL2jAAdj9ZKohwsyIoluQ7WTzGsRPTOTEFzCps76rBTiCCY3LK+mSOlcEEUvahksGQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
