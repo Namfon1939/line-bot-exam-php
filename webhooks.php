59a5ae4be84f6e879cfca7ea15188570bd29e394
Supapit Mekheed <Supapit.m@avalue.co.th>
2020-03-16 13:57:56 +0700

    test22

--- webhooks.php
+++ webhooks.php
@@ -103,54 +103,54 @@ if (!is_null($events['events'])) {
 						]
 					];
 				}
-				if($key > 4){
-					$dataja = [
-						"type" => "bubble",
-						"direction" => "ltr",
-						"header" => [
-							"type" => "box",
-							"layout" => "vertical",
-							"contents" => [
-								[
-									"type" => "spacer",
-									"size" => "xxl"
-								]
-							]
-						],
-						"hero" => [
-							"type" => "image",
-							"url" => "https://firebasestorage.googleapis.com/v0/b/trackhilight.appspot.com/o/more.png?alt=media&token=29e4d90e-465a-4ea7-b4f2-b01df46b43d6",
-							"align" => "center",
-							"gravity" => "center",
-							"size" => "xl",
-							"aspectRatio" => "1:1",
-							"backgroundColor" => "#FFFFFF",
-							"action" => [
-								"type" => "uri",
-								"label" => "ทั้งหมด",
-								"uri" => "line://app/1639925368-Pqw2Qd0O/?shop_id=1"
-							]
-						],
-						"body" => [
-							"type" => "box",
-							"layout" => "vertical",
-							"contents" => [
-								[
-									"type" => "spacer"
-								]
-							]
-						],
-						"footer" => [
-							"type" => "box",
-							"layout" => "horizontal",
-							"contents" => [
-								[
-									"type" => "spacer"
-								]
-							]
-						]
-					];
-				}
+				// if($key > 4){
+				// 	$dataja[5] = [
+				// 		"type" => "bubble",
+				// 		"direction" => "ltr",
+				// 		"header" => [
+				// 			"type" => "box",
+				// 			"layout" => "vertical",
+				// 			"contents" => [
+				// 				[
+				// 					"type" => "spacer",
+				// 					"size" => "xxl"
+				// 				]
+				// 			]
+				// 		],
+				// 		"hero" => [
+				// 			"type" => "image",
+				// 			"url" => "https://firebasestorage.googleapis.com/v0/b/trackhilight.appspot.com/o/more.png?alt=media&token=29e4d90e-465a-4ea7-b4f2-b01df46b43d6",
+				// 			"align" => "center",
+				// 			"gravity" => "center",
+				// 			"size" => "xl",
+				// 			"aspectRatio" => "1:1",
+				// 			"backgroundColor" => "#FFFFFF",
+				// 			"action" => [
+				// 				"type" => "uri",
+				// 				"label" => "ทั้งหมด",
+				// 				"uri" => "line://app/1639925368-Pqw2Qd0O/?shop_id=1"
+				// 			]
+				// 		],
+				// 		"body" => [
+				// 			"type" => "box",
+				// 			"layout" => "vertical",
+				// 			"contents" => [
+				// 				[
+				// 					"type" => "spacer"
+				// 				]
+				// 			]
+				// 		],
+				// 		"footer" => [
+				// 			"type" => "box",
+				// 			"layout" => "horizontal",
+				// 			"contents" => [
+				// 				[
+				// 					"type" => "spacer"
+				// 				]
+				// 			]
+				// 		]
+				// 	];
+				// }
 			}
 
 			$service_json = [
